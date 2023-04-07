1e09a5711614c5fb705219483185685502b49d53
Michael André <128604861+MichaelCuadros@users.noreply.github.com>
2023-04-07 18:24:11 -0500

    Create Damnificado.ts

---
+++ component/Damnificado.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-Damnificado',
+  templateUrl: './Damnificado.component.html',
+  styleUrls: ['./Damnificado.component.css']
+})
+export class DamnificadoComponent implements OnInit{
+
+constructor(public route:ActivatedRoute){
+
+}
+  ngOnInit(): void {
+
+  }
+}
