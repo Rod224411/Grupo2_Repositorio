6e3571d2c48b899b0544947bafa1622cc14b0ebe
Michael André <128604861+MichaelCuadros@users.noreply.github.com>
2023-04-07 18:23:30 -0500

    Create Damnificado.ts

---
+++ model/Damnificado.ts
@@ -0,0 +1,10 @@
+export class Damnificado{
+  dni:number=0
+  nombres:string=""
+  apellidos:string=""
+  edad:number=0
+  peso:number=0
+  telefono:number=0
+  contrasena:string=""
+  correo:string=""
+}
