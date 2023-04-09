ec4dc53339910c34a4e1733e62a9be7c3a5963f7
Rod224411 <103529855+Rod224411@users.noreply.github.com>
2023-04-09 12:15:13 -0500

    Donador

---
+++ model/Donador.ts
@@ -0,0 +1,10 @@
+export class Donador{
+  dni:number=0
+  nombres:string=""
+  apellidos:string=""
+  edad:number=0
+  telefono:number=0
+  contrasena:string=""
+  correo:string=""
+  ong:boolean=false
+}
