3692464d5b68317f529b2bcf65d12aec1459a129
Michael André <128604861+MichaelCuadros@users.noreply.github.com>
2023-04-11 23:05:08 -0500

    Update Damnificado.ts

--- service/Damnificado.ts
+++ service/Damnificado.ts
@@ -1,17 +1,30 @@
-
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Damnificado } from '../model/Damnificado';
+import { Subject } from 'rxjs';
 
-const base_url=environment.base
+const base_url = environment.base;
 @Injectable({
   providedIn: 'root'
 })
 export class DamnificadoService {
-private url=`${base_url}/Damnificados`
-  constructor(private http:HttpClient ) { }
-  list(){
+  private url = `${base_url}/Damnificados`
+  private listaCambio = new Subject<Damnificado[]>()
+  constructor(private http: HttpClient) {}
+  list() {
     return this.http.get<Damnificado[]>(this.url);
+  }
+
+  insert(damnificado:Damnificado) {
+    return this.http.post(this.url, damnificado);
+  }
+
+  setList(ListaNueva:Damnificado[]) {
+    this.listaCambio.next(ListaNueva);
+  }
+
+  getList() {
+    return this.listaCambio.asObservable();
   }
 }
