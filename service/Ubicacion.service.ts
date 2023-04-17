de9656c7db4d20b6c20783e4a5d5a50ff2746c8c
Victormrl-17 <116858543+Victormrl-17@users.noreply.github.com>
2023-04-17 16:39:27 -0500

    Update Ubicacion.service.ts

--- service/Ubicacion.service.ts
+++ service/Ubicacion.service.ts
@@ -2,6 +2,7 @@ import {HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Ubicacion } from '../model/Ubicacion';
+import { Subject } from 'rxjs';
 
 const base_url=environment.base
 @Injectable({
@@ -9,8 +10,18 @@ const base_url=environment.base
 })
 export class UbicacionService {
 private url=`${base_url}/Ubicaciones`
+private listaCambio= new Subject<Ubicacion[]>()
   constructor(private http:HttpClient ) { }
   list(){
     return this.http.get<Ubicacion[]>(this.url);
+  }
+  insert(ubicacion:Ubicacion){
+    return this.http.post(this.url,ubicacion);
+  }
+  setList(listaNueva:Ubicacion[]){
+this.listaCambio.next(listaNueva);
+  }
+  getList(){
+    return this.listaCambio.asObservable();
   }
 }
