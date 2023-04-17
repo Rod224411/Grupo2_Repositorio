beef2e17ea94c289c8f2df7b851604195a071448
Rod224411 <103529855+Rod224411@users.noreply.github.com>
2023-04-17 17:21:07 -0500

    Update Donador.service.ts

--- service/Donador.service.ts
+++ service/Donador.service.ts
@@ -13,4 +13,15 @@ private url=`${base_url}/Donadores`
   list(){
     return this.http.get<Donador[]>(this.url);
   }
+  insert(donador:Donador) {
+    return this.http.post(this.url, donador);
+  }
+
+  setList(ListaNueva:Donador[]) {
+    this.listaCambio.next(ListaNueva);
+  }
+
+  getList() {
+    return this.listaCambio.asObservable();
+  }
 }
