e4e8bb99fc17dac1b86d6f880450566b5fce30eb
MarcelU20 <106358277+MarcelU20@users.noreply.github.com>
2023-04-25 16:34:22 -0500

    Update cuentabancaria.ts

--- service/cuentabancaria.ts
+++ service/cuentabancaria.ts
@@ -1,24 +1,23 @@
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { cuentabancaria } from '../model/cuentabancaria';
 import { Subject } from 'rxjs';
 
-const base_url=environment.base
+const base_url = environment.base;
 @Injectable({
   providedIn: 'root'
 })
-
 export class cuentabancariaService {
-private url=`${base_url}/cuentabancarias`
- private listaCambio = new Subject<cuentabancaria[]>()
+  private url = `${base_url}/cuentabancarias`
+  private listaCambio = new Subject<cuentabancaria[]>()
   constructor(private http: HttpClient) {}
   list() {
     return this.http.get<cuentabancaria[]>(this.url);
   }
 
-  insert(CuentaBancaria:cuentabancaria) {
-   return this.http.post(this.url, CuentaBancaria);
+  insert(cuentabancaria:cuentabancaria) {
+    return this.http.post(this.url, cuentabancaria);
   }
 
   setList(ListaNueva:cuentabancaria[]) {
@@ -28,12 +27,19 @@ private url=`${base_url}/cuentabancarias`
   getList() {
     return this.listaCambio.asObservable();
   }
-     
+
+
+  //lo que se agrega para el editar
   listId(id: number) {
     return this.http.get<cuentabancaria>(`${this.url}/${id}`);
   }
 
-  update(aut: cuentabancaria) { //http HttpClientModule: get-post-put-delete, 
+  update(aut: cuentabancaria) { //http HttpClientModule: get-post-put-delete, hacer cuadro comparativo
     return this.http.put(this.url + '/' + aut.id, aut);
+  }
+
+  //para eliminar damnificado
+  delete(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
   }
 }
